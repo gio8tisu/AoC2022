f340ccc0237ae96a0d729adb4dde42b5e4040501
Sergio Garcia Campderrich <gio8.tisu@gmail.com>
2022-12-12 16:34:26 +0100

    Add day 10 part 2

--- day10/main.py
+++ day10/main.py
@@ -17,11 +17,15 @@ class Instruction:
 
 
 def main(program_file: TextIO, part: int):
+    program = read_program(program_file)
     if part == 1:
-        program = read_program(program_file)
         interesting_cycles = [20, 60, 100, 140, 180, 220]
-        x_values = execute_program(program, interesting_cycles)
+        x_values = execute_program_part_1(program, interesting_cycles)
         print(sum(c * x for c, x in zip(interesting_cycles, x_values)))
+    elif part == 2:
+        display = execute_program_part_2(program)
+        print()
+        print(display)
     else:
         raise ValueError(f"Invalid part: {part}")
 
@@ -40,7 +44,7 @@ def read_program(motions_file: TextIO) -> list[Instruction]:
     return instructions
 
 
-def execute_program(program: list[Instruction], interesting_cycles: list[int]) -> list[int]:
+def execute_program_part_1(program: list[Instruction], interesting_cycles: list[int]) -> list[int]:
     interesting_cycles_x_value = []
     x = 1
     clock = 1
@@ -60,6 +64,34 @@ def execute_program(program: list[Instruction], interesting_cycles: list[int]) -
     if clock in interesting_cycles:
         interesting_cycles_x_value.append(x)
     return interesting_cycles_x_value
+
+
+def execute_program_part_2(program: list[Instruction]) -> str:
+    display = ""
+    x = 1
+    clock = 1
+    program_iter = iter(program)
+    instruction = next(program_iter, None)
+    while instruction is not None:
+        display += draw_pixel(x, clock)
+        if instruction.type == InstructionType.ADD_X:
+            clock += 1
+            display += draw_pixel(x, clock)
+            assert instruction.argument is not None
+            x += instruction.argument
+        instruction = next(program_iter, None)
+        clock += 1
+    return display
+
+
+def draw_pixel(x: int, clock: int) -> str:
+    result = "."
+    c = (clock - 1) % 40
+    if x - 1 <= c <= x + 1:
+        result = "#"
+    if c == 39:
+        result += "\n"
+    return result
 
 
 if __name__ == "__main__":
--- day10/test.py
+++ day10/test.py
@@ -1,6 +1,6 @@
 import unittest
 
-from main import Instruction, InstructionType, execute_program
+from main import Instruction, InstructionType, draw_pixel, execute_program_part_1
 
 
 class TestExecuteProgram(unittest.TestCase):
@@ -10,9 +10,51 @@ class TestExecuteProgram(unittest.TestCase):
             Instruction(InstructionType.ADD_X, 3),
             Instruction(InstructionType.ADD_X, -5),
         ]
-        x = execute_program(program, [1, 2, 3, 4, 5, 6])
+        x = execute_program_part_1(program, [1, 2, 3, 4, 5, 6])
         self.assertEqual(x, [1, 1, 1, 4, 4, -1])
 
+
+
+class TestDrawPixel(unittest.TestCase):
+    def test_draw_pixel_on_sprite_1(self):
+        draw = draw_pixel(1, 1)
+        self.assertEqual(draw, "#")
+
+    def test_draw_pixel_on_sprite_0(self):
+        draw = draw_pixel(0, 1)
+        self.assertEqual(draw, "#")
+
+    def test_draw_pixel_on_sprite_2(self):
+        draw = draw_pixel(2, 2)
+        self.assertEqual(draw, "#")
+
+    def test_draw_pixel_off_sprite(self):
+        draw = draw_pixel(2, 10)
+        self.assertEqual(draw, ".")
+
+    def test_draw_pixel_on_sprite_clock_40(self):
+        draw = draw_pixel(39, 40)
+        self.assertEqual(draw, "#\n")
+
+    def test_draw_pixel_off_sprite_clock_41(self):
+        draw = draw_pixel(1, 41)
+        self.assertEqual(draw, "#")
+
+    def test_draw_pixel_off_sprite_clock_40(self):
+        draw = draw_pixel(20, 41)
+        self.assertEqual(draw, ".")
+
+    def test_draw_pixel_on_sprite_clock_80(self):
+        draw = draw_pixel(39, 80)
+        self.assertEqual(draw, "#\n")
+
+    def test_draw_pixel_on_sprite_clock_81(self):
+        draw = draw_pixel(1, 81)
+        self.assertEqual(draw, "#")
+
+    def test_draw_pixel_off_sprite_clock_81(self):
+        draw = draw_pixel(20, 81)
+        self.assertEqual(draw, ".")
 
 
 if __name__ == "__main__":
