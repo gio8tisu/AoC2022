46b889d0d976427efd9b966669d65eebd7d971d2
Sergio Garcia Campderrich <gio8.tisu@gmail.com>
2022-12-16 15:06:00 +0100

    Add day 14 part 2

--- day14/main.py
+++ day14/main.py
@@ -8,13 +8,13 @@ Coordinate: TypeAlias = tuple[int, int]
 
 def main(cave_rocks_file: TextIO, part: int):
     if part == 1:
-        cave = create_cave(cave_rocks_file)
-        sand_grains = start_sand_pouring(cave)
-        print(sand_grains)
+        cave = create_cave(cave_rocks_file, False)
     elif part == 2:
-        raise NotImplementedError("Part 2 not implemented yet")
+        cave = create_cave(cave_rocks_file, True)
     else:
         raise ValueError(f"Invalid part: {part}")
+    start_sand_pouring(cave)
+    print(cave.sand_grains)
 
 
 class FlowIntoAbyss(Exception):
@@ -22,12 +22,15 @@ class FlowIntoAbyss(Exception):
 
 
 class Cave:
-    rocks: set[Coordinate]
-    sand: set[Coordinate]
+    obstacles: set[Coordinate]
+    sand_grains: int
+    has_floor: bool
+    floor_level = float("inf")
 
-    def __init__(self):
-        self.rocks = set()
-        self.sand = set()
+    def __init__(self, has_floor: bool = False):
+        self.obstacles = set()
+        self.sand_grains = 0
+        self.has_floor = has_floor
 
     def add_rock_structure(self, path: list[Coordinate]):
         start = None
@@ -37,16 +40,18 @@ class Cave:
             else:
                 self._add_rock_line(start, coordinate)
                 start = coordinate
+        if self.has_floor:
+            self.floor_level = max(y for _, y in self.obstacles) + 2
 
     def _add_rock_line(self, start: Coordinate, end: Coordinate):
         x1, y1 = start
         x2, y2 = end
         if x1 == x2:
             for y in range(min(y1, y2), max(y1, y2) + 1):
-                self.rocks.add((x1, y))
+                self.obstacles.add((x1, y))
         elif y1 == y2:
             for x in range(min(x1, x2), max(x1, x2) + 1):
-                self.rocks.add((x, y1))
+                self.obstacles.add((x, y1))
         else:
             raise ValueError("Not a line")
 
@@ -54,22 +59,22 @@ class Cave:
         sand_grain = (500, 0)
         while True:
             x, y = sand_grain
-            if not any(y < y2 for _, y2 in self.rocks):
+            if not self.has_floor and not any(y < y2 for _, y2 in self.obstacles):
                 raise FlowIntoAbyss()
-            if not (x, y + 1) in self.rocks | self.sand:
+            if not (x, y + 1) in self.obstacles and y + 1 < self.floor_level:
                 sand_grain = (x, y + 1)
-            elif not (x - 1, y + 1) in self.rocks | self.sand:
+            elif not (x - 1, y + 1) in self.obstacles and y + 1 < self.floor_level:
                 sand_grain = (x - 1, y + 1)
-            elif not (x + 1, y + 1) in self.rocks | self.sand:
+            elif not (x + 1, y + 1) in self.obstacles and y + 1 < self.floor_level:
                 sand_grain = (x + 1, y + 1)
             else:
                 break
-        self.sand.add(sand_grain)
+        self.obstacles.add(sand_grain)
+        self.sand_grains += 1
 
 
-def create_cave(cave_rocks_file: TextIO) -> Cave:
-    # Example line: 498,4 -> 498,6 -> 496,6
-    cave = Cave()
+def create_cave(cave_rocks_file: TextIO, has_floor: bool) -> Cave:
+    cave = Cave(has_floor)
     for line in cave_rocks_file:
         path = []
         for coordinate in line.strip().split(" -> "):
@@ -79,13 +84,12 @@ def create_cave(cave_rocks_file: TextIO) -> Cave:
     return cave
 
 
-def start_sand_pouring(cave: Cave) -> int:
-    while True:
+def start_sand_pouring(cave: Cave):
+    while (500, 0) not in cave.obstacles:
         try:
             cave.pour_sand()
         except FlowIntoAbyss:
             break
-    return len(cave.sand)
 
 
 if __name__ == "__main__":
--- day14/test.py
+++ day14/test.py
@@ -1,4 +1,5 @@
 import unittest
+from copy import copy
 
 from main import Cave, FlowIntoAbyss
 
@@ -8,51 +9,62 @@ class TestCave(unittest.TestCase):
         cave = Cave()
         path = [(0, 0), (2, 0), (2, 2)]
         cave.add_rock_structure(path)
-        self.assertEqual(cave.rocks, {(0, 0), (1, 0), (2, 0), (2, 1), (2, 2)})
+        self.assertEqual(cave.obstacles, {(0, 0), (1, 0), (2, 0), (2, 1), (2, 2)})
 
     def test_pour_sand_straight_down_rock(self):
         cave = Cave()
-        cave.rocks = {(498, 9), (499, 9), (500, 9), (501, 9), (502, 9)}
+        cave.obstacles = {(498, 9), (499, 9), (500, 9), (501, 9), (502, 9)}
+        expected = copy(cave.obstacles) | {(500, 8)}
         cave.pour_sand()
-        self.assertEqual(cave.sand, {(500, 8)})
+        self.assertEqual(cave.obstacles, expected)
 
     def test_pour_sand_left_rock(self):
         cave = Cave()
-        cave.rocks = {(498, 9), (499, 9), (500, 9), (500, 8), (501, 9), (502, 9)}
+        cave.obstacles = {(498, 9), (499, 9), (500, 9), (500, 8), (501, 9), (502, 9)}
+        expected = copy(cave.obstacles) | {(499, 8)}
         cave.pour_sand()
-        self.assertEqual(cave.sand, {(499, 8)})
+        self.assertEqual(cave.obstacles, expected)
 
     def test_pour_sand_right_rock(self):
         cave = Cave()
-        cave.rocks = {(498, 9), (499, 9), (499, 9), (499, 8), (500, 9), (500, 8), (501, 9), (502, 9)}
+        cave.obstacles = {(498, 9), (499, 9), (499, 9), (499, 8), (500, 9), (500, 8), (501, 9), (502, 9)}
+        expected = copy(cave.obstacles) | {(501, 8)}
         cave.pour_sand()
-        self.assertEqual(cave.sand, {(501, 8)})
+        self.assertEqual(cave.obstacles, expected)
 
     def test_pour_sand_straight_down_sand(self):
         cave = Cave()
-        cave.rocks = {(498, 9), (499, 9), (499, 10), (500, 10), (501, 10), (501, 9), (502, 9)}
-        cave.sand = {(500, 9)}
+        cave.obstacles = {(498, 9), (499, 9), (499, 10), (500, 10), (501, 10), (501, 9), (502, 9), (500, 9)}
+        expected = copy(cave.obstacles) | {(500, 9), (500, 8)}
         cave.pour_sand()
-        self.assertEqual(cave.sand, {(500, 9), (500, 8)})
+        self.assertEqual(cave.obstacles, expected)
 
     def test_pour_sand_left_sand(self):
         cave = Cave()
-        cave.rocks = {(498, 9), (499, 9), (500, 9), (501, 9), (502, 9)}
-        cave.sand = {(500, 8)}
+        cave.obstacles = {(498, 9), (499, 9), (500, 9), (501, 9), (502, 9), (500, 8)}
+        expected = copy(cave.obstacles) | {(500, 8), (499, 8)}
         cave.pour_sand()
-        self.assertEqual(cave.sand, {(500, 8), (499, 8)})
+        self.assertEqual(cave.obstacles, expected)
 
     def test_pour_sand_right_sand(self):
         cave = Cave()
-        cave.rocks = {(498, 9), (499, 9), (500, 9), (501, 9), (502, 9)}
-        cave.sand = {(499, 8), (500, 8)}
+        cave.obstacles = {(498, 9), (499, 9), (500, 9), (501, 9), (502, 9), (499, 8), (500, 8)}
+        expected = copy(cave.obstacles) | {(499, 8), (500, 8), (501, 8)}
         cave.pour_sand()
-        self.assertEqual(cave.sand, {(499, 8), (500, 8), (501, 8)})
+        self.assertEqual(cave.obstacles, expected)
 
     def test_pour_sand_flow_into_abyss(self):
         cave = Cave()
         with self.assertRaises(FlowIntoAbyss):
             cave.pour_sand()
+
+    def test_cave_with_floor(self):
+        cave = Cave(has_floor=True)
+        cave.floor_level = 11
+        cave.obstacles = {(498, 9)}
+        expected = copy(cave.obstacles) | {(500, 10)}
+        cave.pour_sand()
+        self.assertEqual(cave.obstacles, expected)
 
 
 if __name__ == "__main__":
